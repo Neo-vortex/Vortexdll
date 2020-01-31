4373b680ef5cf18510ba27634984dacdb9643908
Mohamadreza Nakhleh <mohamadreza_nakhleh@protonmail.ch>
2020-02-01 01:15:41 +0330

    Initial commit

---
+++ Class1.vb
@@ -0,0 +1,15 @@
+﻿Imports MessagePack
+<MessagePackObject>
+Public Class Vorlib
+    <Key(0)>
+    Public Overridable Property Files As New List(Of Byte())
+    <Key(1)>
+    Public Overridable Property Comment As String
+    <Key(2)>
+    Public Overridable Property Names As New List(Of String)
+    <Key(3)>
+    Public Overridable Property Formats As New List(Of String)
+
+    Public Sub New()
+    End Sub
+End Class
